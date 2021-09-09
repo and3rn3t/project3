a027a62a48f19872235d67719c507cc035e53b5f
smittyill <smithtodda@johndeere.com>
2021-09-09 09:03:35 -0500

    updated stuff

--- eventsfinder/src/App.js
+++ eventsfinder/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {},
+      data: [],
     };
   }
 
--- eventsfinder/src/Components/EventList/EventList.js
+++ eventsfinder/src/Components/EventList/EventList.js
@@ -1,31 +1,49 @@
 import React, { Component } from "react";
 import "./EventList.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class EventList extends Component {
-  render(){
-    console.log(this.props.events)
-    //const eventsArray = this.props.events.map((value, index) => {
-      const eventsList = Object.keys(this.props.data);
-      let eventsArray = eventsList.map((value, index) => {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    if (Object.keys(this.props.data)) {
+      console.log(`props data ${this.props.data}`)
+      console.log(this.props.data)
+      console.log("props data keys" + Object.keys(this.props.data))
+      //const eventsArray = this.props.events.map((value, index) => {
+      //const eventsList = Object.keys(this.props.data);
+
+      let eventsArray = this.props.data.map((value, index) => {
+        console.log("value" + value)
+        // console.log("value.images"+ value.images)
+        //  console.log("value.images[0]"+ value.images[0])
+        // console.log("value.images[0]"+ value.images[0])
+
+
         return (
-            <div key={index}>
-                <Link to={'/event/'+ index} >
-                    <h2>{value.name}</h2>
-                </Link>
-                {/* <img src={value.images[0].url} alt=''/>   */}
-            </div>
+          <div key={index}>
+            <Link to={'/event/' + index} >
+              <h2>{value.name}</h2>
+            </Link>
+            {<img src={value.images[0].url} alt=''/>    }
+          </div>
         )
-    }
-    )
-    console.log(eventsArray)
-    return (
+      }
+      )
+      
+      return (
         <div>
-            <h2>Event List Component</h2>
-            {eventsArray}
+          <h2>Event List Component</h2>
+        
         </div>
-    )
-}
+      )
+
+
+    }
+
+  }
 }
 
 export default EventList;
