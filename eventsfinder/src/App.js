69c97be34a854c560b9514a8a97e3dadeb09d83c
Matthew Anderson <andersonmatthewj@johndeere.com>
2021-09-10 14:55:07 -0500

    refactored search into Search.js

--- eventsfinder/src/App.js
+++ eventsfinder/src/App.js
@@ -1,112 +1,22 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import axios from "axios";
 
 import "./App.css";
 
 import Header from "./Components/Header/header";
-import EventList from "./Components/EventList/EventList";
-import Event from "./Components/Event/Event";
-
-const something = "Fbt32TPxBuKL7RiAXrlZacb7PK45Xg6L";
+import Search from "./Components/Search/search";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data: [],
-      postalCode: "",
-      city: "",
-      keyword: "",
-    };
+    this.state = { data: [] };
   }
-
-  // Takes changes made in Postal Code input field and sets state / runs axios API to generate results based on postal code
-  onPstlChangeHandler = async (e) => {
-    if (e.target.value.length === 5) {
-      this.searchPstl(e.target.value);
-      this.setState({ postalCode: e.target.value });
-    }
-  };
-
-  searchPstl = async (postalCode) => {
-    let results = await axios.get(
-      `https://app.ticketmaster.com/discovery/v2/events.json?postalCode=${postalCode}&apikey=${something}`
-    );
-
-    let events = <h1>No Events Found</h1>;
-    if (results.data._embedded.events) {
-      events = <EventList data={this.state.data} />;
-      this.setState({ data: results.data._embedded.events });
-    }
-    return events;
-  };
-
-  // // Takes changes made in City input field and sets state / runs axios API to generate results based on city
-  // onCityChangeHandler = async (e) => {
-  //   this.searchCity(e.target.city);
-  //   this.setState({ city: e.target.city });
-  // };
-
-  // searchCity = async (city) => {
-  //   let results = await axios.get(
-  //     `https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&apikey=${something}`
-  //   );
-  //   this.setState({ data: results.data._embedded.events });
-  // };
-
-  // // Takes changes made in Keyword input field and sets state / runs axios API to generate results based on keyword
-  // onKeywordChangeHandler = async (e) => {
-  //   this.searchKeyword(e.target.keyword);
-  //   this.setState({ keyword: e.target.keyword });
-  // };
-
-  // searchKeyword = async (keyword) => {
-  //   let results = await axios.get(
-  //     `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${keyword}&apikey=${something}`
-  //   );
-  //   this.setState({ data: results.data._embedded.events });
-  // };
 
   render() {
     return (
       <div>
         <Header />
-        <div className="background">
-          <div>
-            <form>
-              <input
-                type="text"
-                value={this.state.postalCode}
-                onChange={(e) => this.onPstlChangeHandler(e)}
-                placeholder="Enter a ZIP/Postal Code"
-              />
-              {/* <input
-              type="text"
-              value={this.state.city}
-              onChange={(e) => this.onCityChangeHandler(e)}
-              placeholder="Enter a City"
-            />
-            <input
-              type="text"
-              value={this.state.keyword}
-              onChange={(e) => this.onKeywordChangeHandler(e)}
-              placeholder="Enter a keyword"
-            /> */}
-              {/* <button>Reset</button> */}
-            </form>
-            {this.renderEvents}
-          </div>
-          <Route
-            exact
-            path="/"
-            render={() => <EventList data={this.state.data} />}
-          />
-          <Route
-            path="/event/:id"
-            render={(props) => <Event {...props} data={this.state.data} />}
-          />
-        </div>
+        <Search />
+        <div className="background"></div>
       </div>
     );
   }
